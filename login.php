c9960f6ee4285a0b9e071e8d43ababd98678fec9
Luisgarcav <114874130+Luisgarcav@users.noreply.github.com>
2022-11-23 17:00:51 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$dir = 'identifier.sqlite';
+$dir = 'sqlite:identifier.sqlite';
 $dbh  = new PDO($dir) or die("cannot open the database");
 $query =  "SELECT * FROM login WHERE usuario = '" . $nombre . "' and password = '" . $pass . "'";
 foreach ($dbh->query($query) as $row)
