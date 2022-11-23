0cc45ee616716ae8af7784114cf5adde5bece45f
Luisgarcav <114874130+Luisgarcav@users.noreply.github.com>
2022-11-23 16:44:43 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,4 @@
 <?php
-$nombre = $_POST["txtusuario"];
-$dbhost = "137.184.137.130";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "test";
+$dir = '../identifier.sqlite';
+$dbh  = new PDO($dir) or die("cannot open the database");
 
-$conn = sqlite_open('../identifier.sqlite');
-if (!$conn) {
-    die("No hay conexion: ".sqlite_error_string());
-}
-
-$pass = $_POST["txtpassword"];
-
-$query = mysqli_query($conn, "SELECT * FROM login WHERE usuario = '" . $nombre . "' and password = '" . $pass . "'");
-$nr = mysqli_num_rows($query);
-
-if ($nr == 1) {
-    echo "Bienvenido: " . $nombre;
-} else if ($nr == 0) {
-    $com = '<img src = "space-error.svg" width= "100%" height="100%" />';
-    echo $com;
-}
-?>
