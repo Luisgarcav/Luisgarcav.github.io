fe14efcbb031f5bbe13ad170749e92b4ea8f8c5c
Luisgarcav <114874130+Luisgarcav@users.noreply.github.com>
2022-11-23 16:54:50 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
 $dir = 'identifier.sqlite';
 $dbh  = new PDO($dir) or die("cannot open the database");
-
+$query =  "SELECT * FROM login WHERE usuario = '" . $nombre . "' and password = '" . $pass . "'";
+foreach ($dbh->query($query) as $row)
+{
+    echo $row[0];
+}
+$dbh = null; //This is how you close a PDO connection
 ?>
